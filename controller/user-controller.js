c9d5f8676aa4757c14502a9ee24fad63ebfd8a6d
keyap22 <71260297+keyap22@users.noreply.github.com>
2022-03-23 19:37:23 +0530

    role is added in login, compared with role of matched data

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -6,22 +6,22 @@ module.exports.addUser = function (req, res) {         //API
     //db insert role
     //console.log(req.body.roleName)
 
-    let encryptedPassword = bcrypt.hashSync(req.body.password,10)
+    let encryptedPassword = bcrypt.hashSync(req.body.password, 10)
 
     let user = new UserModel({
         email: req.body.email,
-        password : encryptedPassword,
-        mobileNo : req.body.mobileNo,
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
+        password: encryptedPassword,
+        mobileNo: req.body.mobileNo,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         role: req.body.role,
-        profilePhoto : req.body.profilePhoto
+        profilePhoto: req.body.profilePhoto
     })
 
     user.save(function (err, success) {
         if (err) {
             console.log(err);
-            res.json({ msg: "Something Went Wrong", status: -1, data : err })
+            res.json({ msg: "Something Went Wrong", status: -1, data: err })
         }
         else {
             res.json({ msg: "user added successfully", status: 200, data: success })
@@ -42,10 +42,10 @@ module.exports.getAllUsers = function (req, res) {
 }
 
 //delete
-module.exports.deleteUser = function(req, res){
+module.exports.deleteUser = function (req, res) {
 
     let userId = req.params.userId
-    UserModel.deleteOne({"_id" : userId}, function(err, data){
+    UserModel.deleteOne({ "_id": userId }, function (err, data) {
         if (err) {
             res.json({ msg: "Something Went Wrong", status: -1, data: err })
         }
@@ -56,34 +56,40 @@ module.exports.deleteUser = function(req, res){
 }
 
 //update
-module.exports.updateUser = function(req, res){
+module.exports.updateUser = function (req, res) {
     let userId = req.params.userId
     let email = req.body.email
-    let password = bcrypt.hashSync(req.body.password,10)
+    let password = bcrypt.hashSync(req.body.password, 10)
     let mobileNo = req.body.mobileNo
-       let firstName = req.body.firstName
+    let firstName = req.body.firstName
     let lastName = req.body.lastName
-      let  role= req.body.role
-       let profilePhoto = req.body.profilePhoto
+    let role = req.body.role
+    let profilePhoto = req.body.profilePhoto
 
 
-    UserModel.updateOne({"_id" : userId},{ "email": email,"password":password,"mobileNo":mobileNo
-,"firstName":firstName,"lastName":lastName,"profilePhoto":profilePhoto}, function(err, data){
-        if (err) {
-            res.json({ msg: "Something Went Wrong", status: -1, data: err })
-        }
-        else {
-            res.json({ msg: "user is updated successfully", status: 200, data: data })
-        }
-    })
+
+    UserModel.updateOne({ "_id": userId },
+        {
+            "email": email, "password": password, "mobileNo": mobileNo
+            , "firstName": firstName, "lastName": lastName, "profilePhoto": profilePhoto, "role": role
+        }, function (err, data) {
+            if (err) {
+                res.json({ msg: "Something Went Wrong", status: -1, data: err })
+            }
+            else {
+                res.json({ msg: "user is updated successfully", status: 200, data: data })
+            }
+        })
 }
 
+
+
 //get user by userid
-module.exports.getUserById = function(req, res){
-    
+module.exports.getUserById = function (req, res) {
+
     let userId = req.params.userId;
-    
-    UserModel.findById({"_id" : userId}, function(err, data){
+
+    UserModel.findById({ "_id": userId }, function (err, data) {
         if (err) {
             res.json({ msg: "Something Went Wrong", status: -1, data: err })
         }
@@ -94,26 +100,28 @@ module.exports.getUserById = function(req, res){
 }
 
 //login
-module.exports.login = function(req,res){
+module.exports.login = function (req, res) {
 
     let param_email = req.body.email
-    let param_password  = req.body.password 
+    let param_password = req.body.password
+    let param_role = req.body.role
 
-    let isCorrect = false; 
+    let isCorrect = false;
 
-    UserModel.findOne({email:param_email},function(err,data){
-        if(data){
-            let ans =  bcrypt.compareSync(param_password,data.password)
-            if(ans == true){
-                    isCorrect = true
+    UserModel.findOne({ email: param_email}, function (err, data) {
+        if (data) {
+            let ans = bcrypt.compareSync(param_password, data.password)
+            if (ans == true) {
+                if(data.role == param_role){
+                isCorrect = true
+                }
             }
         }
-    
+
         if (isCorrect == false) {
-            res.json({ msg: "Invalid Credentials...", data: req.body, status: -1 })//-1  [ 302 404 500 ]
+            res.json({ msg: "Invalid Credentials...", data: err, status: -1 })//-1  [ 302 404 500 ]
         } else {
-            res.json({ msg: "Login....", data: data, status: 200 })//http status code 
+            res.json({ msg: "Login....", data: data, status: 200, id: data._id })//http status code 
         }
     })
-
 }
